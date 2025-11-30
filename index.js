44bdca2bb3b76eeb096dc2d3c49a83867900c470
MazharAnsari95 <mazharcollage95@gmail.com>
2025-11-30 17:01:34 +0530

    push

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
-let cors=require('cors');
+let cors = require('cors');
 const enquiryRouter = require('./App/routes/web/enquiryRoutes');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get("/api", (req, res) => {
     res.json({ message: "Hello world" });
 });
 
-// Enquiry Routes
 app.use('/api/website/enquiry', enquiryRouter);
 
-// MongoDB URL
-const DBURL = "mongodb://127.0.0.1:27017/userenquirymernmaz";
+const DBURL = "mongodb+srv://mazharcollage95_db_user:uhNqHtBXE0471SW9@test.i89sp2z.mongodb.net/mazhar_database?retryWrites=true&w=majority&appName=test";
+
 const PORT = 8020;
 
-mongoose.connect(DBURL)
-    .then(() => {
-        console.log("✅ Connected to MongoDB");
+mongoose.connect(DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log("✅ Connected to MongoDB");
 
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error(" MongoDB connection error:", err);
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
     });
+})
+.catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+});
